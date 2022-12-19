03734a67debeae4140851ac6f80c40bc4fb078ed
NADESHIKON <allenzhang040817@gmail.com>
2022-12-18 16:02:20 -0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ import DiscordIcon from '@/public/icons/discord.png';
 import styles from './app.module.scss';
 import classNames from 'classnames';
 import TopNavigation from '@/components/navigation/top';
-import NextScript from 'next/script';
 
 export default function RootLayout({
   children,
@@ -20,9 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head>
-          <NextScript strategy="afterInteractive" src="https://arc.io/widget.min.js#ZGU5XFjQ"></NextScript>
-      </head>
       <body>
         <div className="bg-black text-white h-screen w-screen flex flex-col">
             <TopNavigation/>
