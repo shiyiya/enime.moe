9e609eb03c4e093a7a2a3410fd30e1c4c30a6935
CY <shiyiya.11@gmail.com>
2022-12-28 03:52:11 +0800

    Better style for small screen (#7)
    * Upgrade deps
    
    * Better style for small screen
    
    * again
    
    * again

--- app/watch/[anime]/[number]/page.tsx
+++ app/watch/[anime]/[number]/page.tsx
@@ -15,38 +15,71 @@ export default async function Watch({ params }) {
     if (!episode.anime) return <></>
 
     return (
-        <div className={classNames(styles.cont, "my-20 items-stretch")}>
+        <div
+            className={classNames(
+                styles.cont,
+                'my-4 lg:my-20 items-stretch flex-col px-3.5 lg:px-[10vw] lg:py-0 lg:flex-row 1'
+            )}
+        >
             <div className={classNames(styles.episode)}>
-                <EnimePlayer className="relative w-full aspect-video mb-8" episode={episode}/>
+                <EnimePlayer className="relative w-full aspect-video mb-8" episode={episode} />
                 <div className="m-2">
                     <NextLink href={`/anime/${episode.anime.slug}`} prefetch={false}>
-                        <span className="text-3xl">{ title(episode.anime.title) }</span>
+                        <span className="text-3xl">{title(episode.anime.title)}</span>
                     </NextLink>
                     <p className="text-xl text-tertiary">
-                        Episode { episode.number }{ episode.title && <span>: { episode.title }</span> }
+                        Episode {episode.number}
+                        {episode.title && <span>: {episode.title}</span>}
                     </p>
                 </div>
             </div>
-            <div className={classNames(styles["next-eps"], "pl-10 flex flex-col items-start justify-start overflow-y-auto m-0 p-0 w-[17.5rem] h-full max-lg:(w-0 hidden)")}>
+            <div
+                className={classNames(
+                    styles['next-eps'],
+                    'lg:pl-10 flex flex-col items-start justify-start mt-2 lg:mt-0 lg:w-[17.5rem] lg:h-full max-lg:(w-0 hidden) w-full'
+                )}
+            >
                 <span className="text-3xl mb-4 pl-3 m-0">Episodes</span>
-                <div className={styles.line}/>
-                <div className={classNames(styles.nextEps, "flex flex-col py-3 p-0 m-0 justify-start overflow-y-auto w-full")}>
-                    {episode.anime.episodes.map(ep => {
+                <div className={styles.line} />
+                <div
+                    className={classNames(
+                        styles.nextEps,
+                        'flex flex-row lg:flex-col py-3 p-0 m-0 justify-start overflow-x-auto lg:overflow-y-auto w-full'
+                    )}
+                >
+                    {episode.anime.episodes.map((ep) => {
                         return (
-                            <NextLink key={ep.id} href={`/watch/[anime]/[number]/`} as={`/watch/${episode.anime.slug}/${ep.number}`} prefetch={false}>
-                                <div className={classNames(styles["next-ep"], "w-full px-1 py-2 relative")}>
-                                    <EpisodeListItem episode={ep}/>
-                                    {ep.number === episode.number && <div className={classNames(styles.currenttext, "absolute top-0 right-0 left-0 bottom-0 bg-[#000000b0] font-bold text-5xl")}>
-                                        Current
-                                        <br/>
-                                        <br/>
-                                    </div>}
+                            <NextLink
+                                key={ep.id}
+                                href={`/watch/[anime]/[number]/`}
+                                as={`/watch/${episode.anime.slug}/${ep.number}`}
+                                prefetch={false}
+                            >
+                                <div
+                                    className={classNames(
+                                        styles['next-ep'],
+                                        'w-full px-1 py-2 relative w-[8.5rem] md:w-[16.5rem] lg:w-full'
+                                    )}
+                                >
+                                    <EpisodeListItem episode={ep} />
+                                    {ep.number === episode.number && (
+                                        <div
+                                            className={classNames(
+                                                styles.currenttext,
+                                                'absolute top-0 right-0 left-0 bottom-0 bg-[#000000b0] font-bold text-3xl'
+                                            )}
+                                        >
+                                            Current
+                                            <br />
+                                            <br />
+                                        </div>
+                                    )}
                                 </div>
                             </NextLink>
                         )
                     })}
                 </div>
-                <div className={styles.line}/>
+                <div className={styles.line} />
             </div>
         </div>
     )
