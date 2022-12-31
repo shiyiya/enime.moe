6281f76ce513a8bb6ddadb5cd248db846bd9a3d0
NADESHIKON <allenzhang040817@gmail.com>
2022-12-31 14:57:17 -0800

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import RecentReleaseComponent from '@/components/recent-release';
 import { enimeApi } from '@/lib/constant';
 import Arrow from '@/components/arrow';
-import AdBlock from '@/components/ad/block';
 
 export default async function Home() {
     const { data, meta } = await (await fetch(enimeApi + "/recent?perPage=100&language=JP", { cache: "no-store" })).json();
@@ -9,10 +8,7 @@ export default async function Home() {
     return (
         <>
             <div className="ml-[1%]">
-                <div className="flex flex-row">
-                    <p className="font-bold text-4xl mt-10 pl-5 mb-2">Recently Released</p>
-                    <AdBlock/>
-                </div>
+                <p className="font-bold text-4xl mt-10 pl-5 mb-2">Recently Released</p>
                 <Arrow>
                     <RecentReleaseComponent data={data} meta={meta}/>
                 </Arrow>
--- components/ad/index.tsx
+++ components/ad/index.tsx
@@ -2,6 +2,6 @@ import Script from 'next/script';
 
 export default function Ad() {
     return <>
-        <Script async strategy="afterInteractive" crossOrigin="anonymous" src="/google.js"/>
+        <Script async strategy="afterInteractive" crossOrigin="anonymous" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2103276838446130"/>
     </>
 }
